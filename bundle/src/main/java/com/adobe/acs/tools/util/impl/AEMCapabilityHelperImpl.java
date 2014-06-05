9eae01495637d760ec6467db65d018555c726f83
David Gonzalez <davidjgonzalez@gmail.com>
2014-06-04 22:33:44 -0400

    #copyright

--- bundle/src/main/java/com/adobe/acs/tools/util/impl/AEMCapabilityHelperImpl.java
+++ bundle/src/main/java/com/adobe/acs/tools/util/impl/AEMCapabilityHelperImpl.java
@@ -1,3 +1,22 @@
+/*
+ * #%L
+ * ACS AEM Tools Bundle
+ * %%
+ * Copyright (C) 2014 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package com.adobe.acs.tools.util.impl;
 
 import com.adobe.acs.tools.util.AEMCapabilityHelper;
@@ -12,8 +31,8 @@ import org.slf4j.LoggerFactory;
 import javax.jcr.RepositoryException;
 
 @Component(
-        label = "ACS AEM Tools - AEM Version Helper",
-        description = "Provides information about the current AEM installation."
+        label = "ACS AEM Tools - AEM Capability Helper",
+        description = "Provides information about the current AEM installation and what it can and can't do."
 )
 @Service
 public class AEMCapabilityHelperImpl implements AEMCapabilityHelper {
