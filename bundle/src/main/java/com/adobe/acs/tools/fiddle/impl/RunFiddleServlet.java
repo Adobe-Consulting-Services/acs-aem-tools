6390d4e98bc5e4c416746aeacf5f2d179baf2c60
david g <davidjgonzalez@gmail.com>
2017-02-07 20:55:25 -0500

    #170 - Moves AEM Fiddle to use new Sling SPI ResourceProvider APIs to be compatible with AEM 6.3

--- bundle/src/main/java/com/adobe/acs/tools/fiddle/impl/RunFiddleServlet.java
+++ bundle/src/main/java/com/adobe/acs/tools/fiddle/impl/RunFiddleServlet.java
@@ -22,7 +22,6 @@ package com.adobe.acs.tools.fiddle.impl;
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.sling.SlingServlet;
-import org.apache.sling.api.SlingConstants;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.request.RequestDispatcherOptions;
@@ -34,8 +33,6 @@ import org.apache.sling.settings.SlingSettingsService;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.ComponentContext;
-import org.osgi.service.event.Event;
-import org.osgi.service.event.EventAdmin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -46,9 +43,7 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import java.io.File;
 import java.io.IOException;
-import java.util.Collections;
 import java.util.Iterator;
-import java.util.Map;
 
 @SuppressWarnings("serial")
 @SlingServlet(resourceTypes = "acs-tools/components/aemfiddle", selectors = "run", methods = "POST")
@@ -68,7 +63,7 @@ public class RunFiddleServlet extends SlingAllMethodsServlet {
     private File fileRoot;
 
     @Reference
-    private EventAdmin eventAdmin;
+    private FiddleResourceProvider fiddleResourceProvider;
 
     @Reference
     private SlingSettingsService slingSettingsService;
@@ -90,9 +85,7 @@ public class RunFiddleServlet extends SlingAllMethodsServlet {
 
         // doing this as a synchronous event so we ensure that
         // the JSP has been invalidated
-        Map<String, String> props = Collections.singletonMap(
-                SlingConstants.PROPERTY_PATH, script.getPath());
-        eventAdmin.sendEvent(new Event(SlingConstants.TOPIC_RESOURCE_CHANGED, props));
+        fiddleResourceProvider.emitFiddleScriptChange(script.getPath());
 
         final RequestDispatcherOptions options = new RequestDispatcherOptions();
         options.setForceResourceType(Constants.PSEDUO_COMPONENT_PATH);
